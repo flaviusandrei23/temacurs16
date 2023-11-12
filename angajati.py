8c427f82bbec291f9b21eed7e4f1beb6e735f62a
flaviusandrei23 <flaviusandrei07@gmail.com>
2023-11-12 19:01:57 +0200

    Implementare : Stergere angajat

--- angajati.py
+++ angajati.py
@@ -5,7 +5,7 @@ def adauga_angajat():
     prenume = input("Prenume: ")
     salariu = input("Salariu: ")
 
-    with open("angajati.csv", "a", newline=" ") as file:
+    with open("angajati.csv", "a") as file:
         writer = csv.writer(file)
         writer.writerow([nume, prenume, salariu])
 
@@ -13,3 +13,23 @@ def adauga_angajat():
 
 if __name__ == "__main__":
     adauga_angajat()
+
+
+import csv
+
+def stergere_angajat():
+    nume_sters = input("Introduceti numele angajatului de sters: ")
+
+    with open("angajati.csv", "r") as file:
+        reader = csv.reader(file)
+        angajati = list(reader)
+        angajati_noi = [angajat for angajat in angajati if angajat[0] != nume_sters]
+
+
+    with open("angajati.csv", "w") as file:
+        writer = csv.writer(file)
+        writer.writerow(angajati_noi)
+
+
+if __name__ == "__main__":
+    stergere_angajat()
